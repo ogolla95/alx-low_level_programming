8578303007ee04a27c47a94d066efd588b15c140
ogolla95 <ogollafred95@gmail.com>
2023-08-30 14:07:22 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
+ *_puts_recursion - function like puts();
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
